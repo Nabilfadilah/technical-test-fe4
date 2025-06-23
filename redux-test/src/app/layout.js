eaebdadb45a39edd48fdd88591bd87e308b415e0
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-06-23 10:55:29 +0700

    Membungkus dengan provider redux

--- redux-test/src/app/layout.js
+++ redux-test/src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Providers from "./Providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +23,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+         {/* membungkus seluruh aplikasi dengan Redux Provider */}
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
